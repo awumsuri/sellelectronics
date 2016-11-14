84fdf78e59c3b7ef0824a625b60e143a7b41e142
root <awumsuri@gmail.com>
2016-11-14 02:58:04 +0000

    clean up and test

--- src/scraper/ScrapperSchedule.js
+++ src/scraper/ScrapperSchedule.js
@@ -19,7 +19,25 @@ function scrape(type){
     child.on('close', function(data){
         console.info("data-close:"+data);
         killProcess();
+	copyResourceFile();
     });
+}
+
+function copyResourceFile() {
+	exec = require("child_process").spawn;
+	child = exec('cp', ['../resource/gazelleData.json', '/var/www/html/resource/']);
+        child.stderr.on('data', function(data){
+            console.info("data-error:"+data);
+        });
+
+        child.stdout.on('data', function(data){
+            console.info("data:"+data);
+        });
+
+        child.on('close', function(data){
+            console.info("data-close COPY COMPLETE:"+data);
+            killProcess();
+        });
 }
 
 function killProcess(){
@@ -40,16 +58,16 @@ function initializeApplication(){
     var rule = new scheduler.RecurrenceRule();
     rule.hour = 5;
     rule.minute = 1;
-    scheduler.scheduleJob("0 39 06 * * *", function(){
-	    console.log("scraping gazelle:"+new Date());
+    scheduler.scheduleJob("0 37 02 * * *", function(){
+	    console.log("scraping updateMackbookPrices gazelle:"+new Date());
 	    scrape("updateMacbookPrices");
     });
-    scheduler.scheduleJob("0 42 03 * * *", function(){
-      console.log("scraping gazelle:"+new Date());
+    scheduler.scheduleJob("0 00 05 * * *", function(){
+      console.log("scraping updateMacbookProPrices gazelle:"+new Date());
       scrape("updateMacbookProPrices");
     });
-    scheduler.scheduleJob("0 42 00 * * *", function(){
-      console.log("scraping gazelle:"+new Date());
+    scheduler.scheduleJob("0 00 08 * * *", function(){
+      console.log("scraping updateMacbookAirPrices gazelle:"+new Date());
       scrape("updateMacbookAirPrices");
     });
 }
